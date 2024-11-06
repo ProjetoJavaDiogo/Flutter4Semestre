34451da7c816de2a98915e8d3c4ed2d9b991f87e
TheuZCoder <matheusrdsilva1@gmail.com>
2024-11-05 21:22:24 -0300

    atualizações

--- portaria_condominio/lib/app/controllers/auth_controller.dart
+++ portaria_condominio/lib/app/controllers/auth_controller.dart
@@ -21,11 +21,23 @@ class AuthController with ChangeNotifier {
   // Login do usuário
   Future<bool> login(String email, String password) async {
     try {
-      await _firebaseAuth.signInWithEmailAndPassword(
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
-      return true;
+
+      // Verifique se o tipo é compatível com `PigeonUserDetails?`
+      var userDetails =
+          userCredential.user; // Ajuste o tipo conforme necessário
+
+      // Certifique-se de que `userDetails` é do tipo `PigeonUserDetails?`
+      if (userDetails != null) {
+        return true;
+      } else {
+        print("Usuário não encontrado");
+        return false;
+      }
     } catch (e) {
       print("Erro ao fazer login: $e");
       return false;
